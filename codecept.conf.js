333ed7c2d0b020718c98b8822a443ab6f8ca18e6
soffanderson <s.btudz@gmail.com>
2023-07-13 06:02:56 +0800

    Perbaikan server: 2 PASSED

--- codecept.conf.js
+++ codecept.conf.js
@@ -12,7 +12,7 @@ exports.config = {
   output: 'e2e/outputs',
   helpers: {
     Puppeteer: {
-      url: 'http://127.0.0.1:8081',
+      url: 'http://127.0.0.1:8080',
       show: true,
       windowSize: '1200x900',
     },
